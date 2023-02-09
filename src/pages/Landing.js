08c21b48a8a24775f85cf171f0c644a76c75d4cd
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-09 15:42:28 +0000

    logo component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,13 @@
 import React from "react";
-import logo from "../assets/images/logo.svg";
+import {Logo} from "../components";
 import main from "../assets/images/main.svg";
-import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobsterlogo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
@@ -26,50 +26,5 @@ const Landing = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -1rem;
-  }
-
-  h1 {
-    font-weight: 700;
-
-    span {
-      color: var(--primary-500);
-    }
-  }
-
-  p {
-    color: var(--grey-600);
-  }
-
-  .main-img {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
