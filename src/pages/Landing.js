7c65285047468509a48592bb9b365a612b04f08c
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-09 15:16:54 +0000

    landing page styled

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,17 +1,18 @@
 import React from "react";
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
-export const Landing = () => {
+const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobsterlogo" className="logo" />
       </nav>
       <div className="container page">
         <div className="info">
           <h1>
-            job<span>tracking</span>app
+            job <span> tracking </span> app
           </h1>
           <p>
             Tell your brand story, increase your followers and engage with
@@ -22,6 +23,53 @@ export const Landing = () => {
         </div>
         <img src={main} alt="main logo" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -1rem;
+  }
+
+  h1 {
+    font-weight: 700;
+
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
