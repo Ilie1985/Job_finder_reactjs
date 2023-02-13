5b75774eea3fa3886b7c370cf2252e6a7f708945
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-13 14:32:00 +0000

    protected route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Landing, Error, Register } from "./pages";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
 import {
   AddJob,
   AllJobs,
@@ -14,9 +14,16 @@ import {
 function App() {
   return (
     <BrowserRouter>
-      <Routes >
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Stats />}/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
           <Route path="/all-jobs" element={<AllJobs />} />
           <Route path="/add-job" element={<AddJob />} />
           <Route path="/profile" element={<Profile />} />
