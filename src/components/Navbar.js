a2c719d7f8ca0e86ed6c04dedb6b199e89d4baa8
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-13 11:53:56 +0000

    toggle sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,17 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDispatch, useSelector } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
 
 const Navbar = () => {
+
+
   const user = useSelector((store) => {
     return store.user;
   });
 
   const dispatch = useDispatch();
+
   return (
     <Wrapper>
       <div className="nav-center">
@@ -17,7 +21,7 @@ const Navbar = () => {
           type="button"
           className="toggle-btn"
           onClick={() => {
-            console.log("toggle-sidebar");
+            dispatch(toggleSidebar());
           }}
         >
           <FaAlignLeft />
